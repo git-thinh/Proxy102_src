edd7de439cbd0e2418e4247c60e6ea66bd562517
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-02-13 15:21:51 +0700

    a

---
+++ _run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Diagnostics.Process.Start(@"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe");
+            System.Diagnostics.Process.Start(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", @"./Proxy102.exe");
+            //Console.ReadLine();
+        }
+    }
+}
